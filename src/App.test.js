3729d6e6d140e4e9f91000063fc27fd1de2265b0
Christian Ekrem <christian.ekrem@07.no>
2018-01-16 16:03:46 +0100

    updated app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* eslint-env jest */
+import React from 'react'
+import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom'
+import App from './App'
+import configureStore from './configureStore'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const store = configureStore()
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  div)
+  ReactDOM.unmountComponentAtNode(div)
+})
